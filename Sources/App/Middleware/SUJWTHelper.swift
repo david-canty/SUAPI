dfc8dd15ab740f1375b850cccb95745a5377294b
David Canty <david.canty@icloud.com>
2018-08-20 19:21:58 +0100

    JWT authentication work in progress

--- Sources/App/Middleware/SUJWTHelper.swift
+++ Sources/App/Middleware/SUJWTHelper.swift
@@ -15,67 +15,129 @@ final class SUJWTHelper {
         
         do {
             
-            // Request public keys
             let client = try app.client()
             let httpReq = HTTPRequest(method: .GET, url: "https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com")
             let req = Request(http: httpReq, using: app)
             let res = client.send(req)
             
+//            let keys = client.send(req)
+//                .flatMap { try $0.content.decode([String: String].self) }
+//            _ = keys.map() { key in
+//                let dictKeys = key.keys
+//                print(dictKeys)
+//            }
+            
             // Parse response
             res.do { response in
                 
-                // Parse cache control max-age
-                if let cacheControlString = response.http.headers.firstValue(name: .cacheControl) {
-
-                    if let maxAgeRange = cacheControlString.range(of: "max-age=") {
-
-                        let cacheSubstringFromMaxAge = cacheControlString[maxAgeRange.upperBound..<cacheControlString.endIndex]
-                        let substringComponents = cacheSubstringFromMaxAge.split(separator: ",")
-                        let maxAgeSubstring = substringComponents[0]
-                        self.cacheControlMaxAge = Int(maxAgeSubstring)!
-                    }
-                }
-
-                // Parse public keys
-                if let bodyData = response.http.body.data {
+                do {
                     
-                    let bodyString = String(data: bodyData, encoding: .utf8)
-                    
-                    if let keyComponentPairs = bodyString?.split(separator: ",") {
-                    
-                        for keyComponents in keyComponentPairs {
+                    // Parse cache control max-age
+                    if let cacheControlString = response.http.headers.firstValue(name: .cacheControl) {
+                        
+                        if let maxAgeRange = cacheControlString.range(of: "max-age=") {
                             
-                            let keyPairs = keyComponents.split(separator: ":")
-                            
-                            let keyId = keyPairs[0]
-                            var keyIdStripped = keyId.replacingOccurrences(of: "\"", with: "")
-                            keyIdStripped = keyIdStripped.replacingOccurrences(of: "{", with: "")
-                            keyIdStripped = keyIdStripped.replacingOccurrences(of: "\n", with: "")
-                            keyIdStripped = keyIdStripped.replacingOccurrences(of: " ", with: "")
-                            
-                            let keyCert = keyPairs[1]
-                            var keyCertStripped = keyCert.replacingOccurrences(of: "\"", with: "")
-                            keyCertStripped = keyCertStripped.replacingOccurrences(of: " ", with: "")
-                            // also } and check anything else
-                            
-                            self.publicKeys[keyIdStripped] = keyCertStripped
+                            let cacheSubstringFromMaxAge = cacheControlString[maxAgeRange.upperBound..<cacheControlString.endIndex]
+                            let substringComponents = cacheSubstringFromMaxAge.split(separator: ",")
+                            let maxAgeSubstring = substringComponents[0]
+                            self.cacheControlMaxAge = Int(maxAgeSubstring)!
                         }
                     }
+                    
+                    // Parse public keys
+                    _ = try response.content.decode([String: String].self).map() { keys in
+                        
+                         self.publicKeys = keys
+                        print(self.publicKeys)
+                    }
+                    
+                    // Schedule next key refresh
+                    _ = self.app.eventLoop.scheduleTask(in: TimeAmount.seconds(self.cacheControlMaxAge)) { () -> Void in
+                        self.fetchPublicKeys()
+                    }
+                    
+                } catch {
+                    
+                    fatalError("Could not decode content: \(error)")
                 }
-                
-                // Schedule next key refresh
-                _ = self.app.eventLoop.scheduleTask(in: TimeAmount.seconds(self.cacheControlMaxAge)) { () -> Void in
-                    self.fetchPublicKeys()
-                }
-                
+
             }.catch { error in
-                
-                fatalError("Could not send HTTP request: \(error)")
+                    
+                fatalError("Could not get HTTP response: \(error)")
             }
-        
+            
         } catch {
             
             fatalError("Could not create HTTP client: \(error)")
         }
     }
+    
+//    func fetchPublicKeys() {
+//
+//        do {
+//
+//            // Request public keys
+//            let client = try app.client()
+//            let httpReq = HTTPRequest(method: .GET, url: "https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com")
+//            let req = Request(http: httpReq, using: app)
+//            let res = client.send(req)
+//
+//            // Parse response
+//            res.do { response in
+//
+//                // Parse cache control max-age
+//                if let cacheControlString = response.http.headers.firstValue(name: .cacheControl) {
+//
+//                    if let maxAgeRange = cacheControlString.range(of: "max-age=") {
+//
+//                        let cacheSubstringFromMaxAge = cacheControlString[maxAgeRange.upperBound..<cacheControlString.endIndex]
+//                        let substringComponents = cacheSubstringFromMaxAge.split(separator: ",")
+//                        let maxAgeSubstring = substringComponents[0]
+//                        self.cacheControlMaxAge = Int(maxAgeSubstring)!
+//                    }
+//                }
+//
+//                // Parse public keys
+//                if let bodyData = response.http.body.data {
+//
+//
+//                    let bodyString = String(data: bodyData, encoding: .utf8)
+//
+//                    if let keyComponentPairs = bodyString?.split(separator: ",") {
+//
+//                        for keyComponents in keyComponentPairs {
+//
+//                            let keyPairs = keyComponents.split(separator: ":")
+//
+//                            let keyId = keyPairs[0]
+//                            var keyIdStripped = keyId.replacingOccurrences(of: "\"", with: "")
+//                            keyIdStripped = keyIdStripped.replacingOccurrences(of: "{", with: "")
+//                            keyIdStripped = keyIdStripped.replacingOccurrences(of: "\n", with: "")
+//                            keyIdStripped = keyIdStripped.replacingOccurrences(of: " ", with: "")
+//
+//                            let keyCert = keyPairs[1]
+//                            var keyCertStripped = keyCert.replacingOccurrences(of: "\"", with: "")
+//                            keyCertStripped = keyCertStripped.replacingOccurrences(of: "}", with: "")
+//                            keyCertStripped = keyCertStripped.replacingOccurrences(of: " ", with: "")
+//
+//                            self.publicKeys[keyIdStripped] = keyCertStripped
+//                        }
+//                    }
+//                }
+//
+//                // Schedule next key refresh
+//                _ = self.app.eventLoop.scheduleTask(in: TimeAmount.seconds(self.cacheControlMaxAge)) { () -> Void in
+//                    self.fetchPublicKeys()
+//                }
+//
+//            }.catch { error in
+//
+//                fatalError("Could not send HTTP request: \(error)")
+//            }
+//
+//        } catch {
+//
+//            fatalError("Could not create HTTP client: \(error)")
+//        }
+//    }
 }
--- Sources/App/Middleware/SUJWTMiddleware.swift
+++ Sources/App/Middleware/SUJWTMiddleware.swift
@@ -18,12 +18,35 @@ final class SUJWTMiddleware: Middleware {
                 throw Abort(HTTPResponseStatus.unauthorized)
             }
             
-            // - validate claims
+            // Validate claims
             
+            
+            // Verify token
             let headerKid = jwt.header[.kid] as! String
-            let publicKey = SUJWTHelper.sharedInstance.publicKeys[headerKid]
             
-            //let verified = try JWT.verify(token, using: .rs256(Data(certificate.utf8), RSAKeyType.certificate) - throw abort if not verified
+            if let publicKey = SUJWTHelper.sharedInstance.publicKeys[headerKid] {
+                
+                let stringFromWeb = "-----BEGIN CERTIFICATE-----\nMIIDHDCCAgSgAwIBAgIIJtpzZgDm7/IwDQYJKoZIhvcNAQEFBQAwMTEvMC0GA1UE\nAxMmc2VjdXJldG9rZW4uc3lzdGVtLmdzZXJ2aWNlYWNjb3VudC5jb20wHhcNMTgw\nODEzMjEyMDI5WhcNMTgwODMwMDkzNTI5WjAxMS8wLQYDVQQDEyZzZWN1cmV0b2tl\nbi5zeXN0ZW0uZ3NlcnZpY2VhY2NvdW50LmNvbTCCASIwDQYJKoZIhvcNAQEBBQAD\nggEPADCCAQoCggEBALfda+cug+JOY+u1uuIOn93JTxDHCWSwhDVgh488tgkPdi8z\ncG6r0r8DaiqRlyJWDcZ9Ywa2IPNHgyEWw07iQACykyZecZy3TIKoP0Bo5Fz7Tb+0\nZw8Qf0veeSksMybwLaVV5Xtkr3IDw3LoESOca0Bd5oHPv2I/3amZIAg2Q0JSuAQs\nQ88f6/zXTVpi1wBmKbCx0hxpPRjMJhCwCKmXqd5a0D6+ythGQy3rFU1qN3IB/7BR\nmYdNPr2N6UUn8AixYkhNE+tV/1Ww2XDWsYfHKTXCcUOtLRc9KWcL+3E8oOpt7785\ni6YgvdiMKz3Pw9bPJcwZKiEjKNNe9pZuqo6RskECAwEAAaM4MDYwDAYDVR0TAQH/\nBAIwADAOBgNVHQ8BAf8EBAMCB4AwFgYDVR0lAQH/BAwwCgYIKwYBBQUHAwIwDQYJ\nKoZIhvcNAQEFBQADggEBAK+RBKB57tihgDeIXQHfW3RFZbZVt1jnsFo04qPf4lRX\n3tk0e/Xk0ZyGx6YXIUGyCH57Gmv8MQJAfjcYXpjfNbBR3VuOAP9Et0E8+HJihoJd\n871ozoxt8PPOYcATWaDy9R7j8YpGlYRqSHjwdLys/4Fg7cYNslDj+KbuNzbgDA18\nkf99K7qj9mGe+vAmDmkoXdmmvx+PSoInpsawNYGpVcNdY7F0tKnr7og0lh5ISeLd\nfSQMwj0YGFph6ZmiXY8rl0nLYLWyP7CaNAoYs7IzK0oraWgmtKXWxSmXCzF3jJ4N\nMIw1J91a0vE77uVms0OHPPJyUkjSpOjRfu0WIMZI130=\n-----END CERTIFICATE-----\n"
+                
+                if stringFromWeb == publicKey {
+                    print("the same")
+                } else {
+                    print("not the same")
+                }
+                
+                let verified = try JWT.verify(token, using: .rs256(Data(publicKey.utf8), RSAKeyType.certificate))
+                
+                if !verified {
+                    
+                    throw Abort(HTTPResponseStatus.unauthorized)
+                }
+                
+            } else {
+                
+                throw Abort(HTTPResponseStatus.unauthorized)
+            }
+            
+            
             
             return res
         }
