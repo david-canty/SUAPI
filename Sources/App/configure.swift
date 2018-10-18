798e9d7b5b716aabacb627254a856438d09d25b8
David Canty <david.canty@icloud.com>
2018-10-18 18:50:37 +0100

    Database config for Cloud 2

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -35,21 +35,35 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     middlewares.use(SessionsMiddleware.self)
     
     services.register(middlewares)
-
-    var databases = DatabasesConfig()
-    let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
-    let databasePort = 3306
-    let username = Environment.get("DATABASE_USER") ?? "suapi"
-    let password = Environment.get("DATABASE_PASSWORD") ?? "password"
-    //let databaseName = Environment.get("DATABASE_DB") ?? "suapi"
-    let databaseName = Environment.get("DB_MYSQL") ?? "suapi"
     
-    let databaseConfig = MySQLDatabaseConfig(
-        hostname: hostname,
-        port: databasePort,
-        username: username,
-        password: password,
-        database: databaseName)
+    var databases = DatabasesConfig()
+    let databaseConfig: MySQLDatabaseConfig
+    
+    let url = Environment.get("DB_MYSQL")
+    
+    if let url = url {
+        
+        guard let urlConfig = try MySQLDatabaseConfig(url: url) else {
+            fatalError("Failed to create MySQLDatabaseConfig")
+        }
+        
+        databaseConfig = urlConfig
+        
+    } else {
+        
+        let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
+        let databasePort = 3306
+        let username = Environment.get("DATABASE_USER") ?? "suapi"
+        let password = Environment.get("DATABASE_PASSWORD") ?? "password"
+        let databaseName = Environment.get("DATABASE_DB") ?? "suapi"
+        
+        databaseConfig = MySQLDatabaseConfig(
+            hostname: hostname,
+            port: databasePort,
+            username: username,
+            password: password,
+            database: databaseName)
+    }
     
     let database = MySQLDatabase(config: databaseConfig)
     databases.add(database: database, as: .mysql)
