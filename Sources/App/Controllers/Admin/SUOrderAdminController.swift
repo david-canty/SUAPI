0d6c6d550419d1463c1aaeaf0ede09053a9ab7a4
David Canty <david.canty@icloud.com>
2018-11-19 12:57:46 +0000

    Orders returning item count

--- Sources/App/Controllers/Admin/SUOrderAdminController.swift
+++ Sources/App/Controllers/Admin/SUOrderAdminController.swift
@@ -11,50 +11,99 @@ struct SUOrderAdminController: RouteCollection {
         let redirectProtectedRoutes = authSessionRoutes.grouped(RedirectMiddleware<SUUser>(path: "/sign-in"))
         
         redirectProtectedRoutes.get(use: ordersHandler)
-        redirectProtectedRoutes.get(SUOrder.parameter, use: viewOrderHandler)
+        //redirectProtectedRoutes.get(SUOrder.parameter, use: viewOrderHandler)
     }
     
     // CRUD handlers
     func ordersHandler(_ req: Request) throws -> Future<View> {
         
         return SUOrder.query(on: req).sort(\.orderDate, .descending).all().flatMap(to: View.self) { orders in
-        
-            var orderList: [OrderDetail] = []
-            for order in orders {
             
-                let customer = order.customer.get(on: req)
-                let orderItems = try order.orderItems.query(on: req).all()
-                let orderDetail = OrderDetail(customer: customer, order: order, orderItems: orderItems)
-                orderList.append(orderDetail)
+            return try orders.compactMap { order in
+                
+                return try order.orderItems.query(on: req).all().map(to: OrderDetail.self) { orderItems in
+                 
+                    var itemCount = 0
+                    for item in orderItems {
+                        itemCount += item.quantity
+                    }
+                    
+                        let customer = order.customer.get(on: req)
+                        return OrderDetail(customer: customer, order: order, orderItems: orderItems, itemCount: itemCount)
+                    
+                    }
+                }.flatMap(to: View.self, on: req) { orderDetails in
+                        
+                        let user = try req.requireAuthenticated(SUUser.self)
+                        let context = OrdersContext(authenticatedUser: user, orderDetails: orderDetails)
+                        
+                        return try req.view().render("orders", context)
             }
-            
-            let user = try req.requireAuthenticated(SUUser.self)
-            let context = OrdersContext(authenticatedUser: user, orderList: orderList)
-            
-            return try req.view().render("orders", context)
         }
     }
     
-    func viewOrderHandler(_ req: Request) throws -> Future<View> {
-        
-        return try req.parameters.next(SUOrder.self).flatMap(to: View.self) { order in
-            
-            let customer = order.customer.get(on: req)
-            let orderItems = try order.orderItems.query(on: req).all()
-            let orderDetail = OrderDetail(customer: customer, order: order, orderItems: orderItems)
-            
-            let user = try req.requireAuthenticated(SUUser.self)
-            let context = ViewOrderContext(authenticatedUser: user, order: orderDetail)
-            
-            return try req.view().render("order", context)
-        }
-    }
+//    func ordersHandler(_ req: Request) throws -> Future<View> {
+//
+//        return SUOrder.query(on: req).sort(\.orderDate, .descending).all().flatMap(to: View.self) { orders in
+//
+//            let orderList = try orders.map { order -> OrderDetail in
+//
+//                let customer = order.customer.get(on: req)
+//                let orderItems = try order.orderItems.query(on: req).all()
+//                return OrderDetail(customer: customer, order: order, orderItems: orderItems)
+//            }
+//
+//            let user = try req.requireAuthenticated(SUUser.self)
+//            let context = OrdersContext(authenticatedUser: user, orderList: orderList)
+//
+//            return try req.view().render("orders", context)
+//
+//        }
+//    }
+    
+//    func ordersHandler(_ req: Request) throws -> Future<View> {
+//
+//        return SUOrder.query(on: req).sort(\.orderDate, .descending).all().flatMap(to: View.self) { orders in
+//        
+//            var orderList: [OrderDetail] = []
+//            for order in orders {
+//
+//                let customer = order.customer.get(on: req)
+//                let orderItems = try order.orderItems.query(on: req).all()
+//
+//
+//
+//                let orderDetail = OrderDetail(customer: customer, order: order, orderItems: orderItems)
+//                orderList.append(orderDetail)
+//            }
+//
+//            let user = try req.requireAuthenticated(SUUser.self)
+//            let context = OrdersContext(authenticatedUser: user, orderList: orderList)
+//
+//            return try req.view().render("orders", context)
+//        }
+//    }
+    
+//    func viewOrderHandler(_ req: Request) throws -> Future<View> {
+//
+//        return try req.parameters.next(SUOrder.self).flatMap(to: View.self) { order in
+//
+//            let customer = order.customer.get(on: req)
+//            let orderItems = try order.orderItems.query(on: req).all()
+//            let orderDetail = OrderDetail(customer: customer, order: order, orderItems: orderItems)
+//
+//            let user = try req.requireAuthenticated(SUUser.self)
+//            let context = ViewOrderContext(authenticatedUser: user, order: orderDetail)
+//
+//            return try req.view().render("order", context)
+//        }
+//    }
     
     // Contexts
     struct OrdersContext: Encodable {
         let title = "Orders"
         let authenticatedUser: SUUser
-        let orderList: [OrderDetail]
+        let orderDetails: [OrderDetail]
     }
     
     struct ViewOrderContext: Encodable {
@@ -66,6 +115,8 @@ struct SUOrderAdminController: RouteCollection {
     struct OrderDetail: Encodable {
         let customer:  EventLoopFuture<SUCustomer>
         let order: SUOrder
-        let orderItems: EventLoopFuture<[SUOrderItem]>
+        let orderItems: [SUOrderItem]
+        let itemCount: Int
+//        let orderTotal: Double
     }
 }
