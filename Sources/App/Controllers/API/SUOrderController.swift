0b47f6c68e076886a07476d91a13ade3ea970f5b
David Canty <david.canty@icloud.com>
2018-11-21 18:21:57 +0000

    Customer create handler move to order controller

--- Sources/App/Controllers/API/SUOrderController.swift
+++ Sources/App/Controllers/API/SUOrderController.swift
@@ -22,12 +22,65 @@ struct SUOrderController: RouteCollection {
         let orderRoutes = router.grouped("api", "customers")
         orderRoutes.group(SUJWTMiddleware.self) { jwtProtectedGroup in
 
-            jwtProtectedGroup.post(SUCustomer.parameter, "orders", use: createHandler)
+            jwtProtectedGroup.post(use: createCustomerHandler)
+            jwtProtectedGroup.post(SUCustomer.parameter, "orders", use: createOrderHandler)
 
         }
     }
+    
+    func createCustomerHandler(_ req: Request) throws -> Future<SUCustomer> {
+        
+        return try req.content.decode(SUCustomerData.self).flatMap(to: SUCustomer.self) { customerData in
+            
+            let firebaseUserId = customerData.firebaseUserId
+            let email = customerData.email
+            
+            let customer = SUCustomer(firebaseUserId: firebaseUserId, email: email)
+            
+            do {
+                
+                try customer.validate()
+                customer.timestamp = Date()
+                
+            } catch {
+                
+                if let validationError = error as? ValidationError {
+                    
+                    let errorString = "Error creating customer:\n\n"
+                    var validationErrorReason = errorString
+                    
+                    if validationError.reason.contains("valid email") {
+                        validationErrorReason += "Invalid email address."
+                    }
+                    
+                    if validationErrorReason != errorString {
+                        throw Abort(.badRequest, reason: validationErrorReason)
+                    }
+                }
+            }
+            
+            return customer.save(on: req).catchMap { error in
+                
+                let errorDescription = error.localizedDescription.lowercased()
+                
+                switch errorDescription {
+                    
+                case let str where str.contains("duplicate"):
+                    throw Abort(.conflict, reason: "Error creating customer:\n\nA customer with this email exists.")
+                    
+                default:
+                    throw Abort(.internalServerError, reason: error.localizedDescription)
+                }
+            }
+        }
+    }
+    
+    struct SUCustomerData: Content {
+        let firebaseUserId: String
+        let email: String
+    }
 
-    func createHandler(_ req: Request) throws -> Future<SUOrderInfo> {
+    func createOrderHandler(_ req: Request) throws -> Future<SUOrderInfo> {
 
         return try flatMap(to: SUOrderInfo.self, req.parameters.next(SUCustomer.self), req.content.decode(SUOrderPostData.self)) { customer, orderData in
             
